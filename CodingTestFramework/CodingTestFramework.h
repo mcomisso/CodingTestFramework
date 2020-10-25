c90669fe7beaed716b5049094c2a4c2d016f7b5f
Matteo Comisso <matteo.comisso@me.com>
2020-10-25 16:51:52 +0000

    Initial Commit

---
+++ CodingTestFramework/CodingTestFramework.h
@@ -0,0 +1,18 @@
+//
+//  CodingTestFramework.h
+//  CodingTestFramework
+//
+//  Created by Matteo Comisso on 25/10/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CodingTestFramework.
+FOUNDATION_EXPORT double CodingTestFrameworkVersionNumber;
+
+//! Project version string for CodingTestFramework.
+FOUNDATION_EXPORT const unsigned char CodingTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodingTestFramework/PublicHeader.h>
+
+
