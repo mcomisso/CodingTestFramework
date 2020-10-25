d1fcb91dfcaa24de15dbe74ee11a6ef29b0c4588
Matteo Comisso <matteo.comisso@me.com>
2020-10-25 18:53:35 +0000

    iso and default JSON decoders

--- CodingTestFramework/Utils/Foundation/JSONDecoder+extensions.swift
+++ CodingTestFramework/Utils/Foundation/JSONDecoder+extensions.swift
@@ -2,10 +2,14 @@ import Foundation
 
 extension JSONDecoder {
     /// A default implementation of JSONDecoder, with .iso8601 date strategy
-    static var `default`: JSONDecoder {
+    static var iso8601: JSONDecoder {
         let d = JSONDecoder()
         d.dateDecodingStrategy = .iso8601
         d.keyDecodingStrategy = .convertFromSnakeCase
         return d
+    }
+
+    static var `default`: JSONDecoder {
+        JSONDecoder()
     }
 }
