533b4edd39b1f0b611a8ca2669e7a323df3a429d
Matteo Comisso <matteo.comisso@me.com>
2020-10-26 17:50:29 +0000

    Remove protocol extension for API

--- CodingTestFramework/Networking/API.swift
+++ CodingTestFramework/Networking/API.swift
@@ -8,13 +8,3 @@ public enum APIError: Error {
 public protocol API {
     var baseAPI: URLComponents { get }
 }
-
-public extension API {
-    var baseAPI: URLComponents {
-
-        guard !LaunchArgumentReader.wasLaunched(.uiTestEnabled) else {
-            return URLComponents(string: "http://localhost:8080/")!
-        }
-        return URLComponents(string: "https://api.gousto.co.uk/products/v2.0/")!
-    }
-}
